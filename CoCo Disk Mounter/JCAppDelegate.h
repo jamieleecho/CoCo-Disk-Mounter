d841a365041b3fb8577a31bd9bc44fbccc199626
Jamie Cho <jamieleecho@gmail.com>
2012-12-28 17:53:12 -0500

    Initial Commit

---
+++ CoCo Disk Mounter/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  CoCo Disk Mounter
+//
+//  Created by Jamie Cho on 2012-12-28.
+//  Copyright (c) 2012 Jamie Cho. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CoCo Disk Mounter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoCo Disk Mounter
+//
+//  Created by Jamie Cho on 2012-12-28.
+//  Copyright (c) 2012 Jamie Cho. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
